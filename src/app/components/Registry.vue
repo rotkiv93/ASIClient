42315dbd30a25b73ca5805631d485ffa667e46be
Pablo <pablestal@gmail.com>
2018-10-31 17:10:47 +0100

    Added registry(Uncomleted)

---
+++ src/app/components/Registry.vue
@@ -0,0 +1,96 @@
+<template>
+  <LoadingPage
+    :loading="loading"
+    :error="error">
+    <div class="margenes">
+      <div class="float-right">
+        <b-btn
+          variant="primary"
+          @click="back()">Back</b-btn>
+        <b-btn
+          variant="primary"
+          @click="save()">Registrate</b-btn>
+      </div>
+
+      <b-form
+        v-if="user"
+        class="app-form"
+        @submit="save">
+
+        <b-form-group
+          label="Login:"
+          label-for="login">
+          <b-form-input
+            id="login"
+            v-model="user.login"
+            type="text"
+            required
+            placeholder="Introduce login"/>
+        </b-form-group>
+
+
+        <b-form-group
+          label="Password:"
+          label-for="password">
+          <b-form-input
+            id="password"
+            v-model="user.password"
+            type="text"
+            required
+            placeholder="Introduce password"/>
+        </b-form-group>
+
+        <b-form-group
+          label="E-mail:"
+          label-for="e-mail">
+          <b-form-input
+            id="email"
+            v-model="user.email"
+            type="text"
+            required
+            placeholder="Introduce e-mail"/>
+        </b-form-group>
+
+    <div class="margenes">
+  </LoadingPage>
+</template>
+
+<script>
+import { HTTP } from '../../common/http-common'
+import LoadingPage from '../../components/LoadingPage'
+
+export default {
+  components: { LoadingPage },
+  data() {
+    return {
+      user: null,
+      error: null,
+      loading: false,
+    }
+  },
+  created() {
+    if (this.$route.params.id) {
+      this.loading = true
+
+      HTTP.get(`users/${this.$route.params.id}`)
+      .then(response => this.user = response.data)
+      .catch(err => this.error = err.message)
+      .finally(() => this.loading = false)
+    } else {
+      this.user = {}
+    }
+  },
+  methods: {
+    save() {
+        HTTP.post('users', this.user)
+        .then(response =>
+          this.$router.replace({ name: 'UserList', params: { id: response.data.id }}))
+        .catch(err => this.error = err.message)
+      }
+    },
+    back() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
