bb3ab6556ef24edf6575374be09124e7e827cdf3
Pablo <pablestal@gmail.com>
2018-11-06 13:46:20 +0100

    Genre view (does not submitt)

--- src/app/entities/genre/GenreList.vue
+++ src/app/entities/genre/GenreList.vue
@@ -2,23 +2,46 @@
   <LoadingPage
     :loading="loading"
     :error="error">
+
     <div class="margenes">
-      <!-- <div class="float-right" >
-        <b-btn
-          :to="{ name: 'GenreCreate' }"
-          variant="outline-primary">New</b-btn>
-      </div> -->
+      <b-btn
+           v-b-toggle.collapseNew
+           variant = "outline-success">New</b-btn>
+           <b-collapse id="collapseNew" class="mt-2">
+            <b-card>
+                Introduce new genre:
+                <b-form-input
+                  id="nombre"
+                  v-model="genre.nombre"
+                  type="text"
+                  required/>
+                <b-btn
+                  variant="success"
+                  @click="save()">Submit</b-btn>
+                </b-card>
+      </b-collapse>
+      <h3>List of genres:</h3>
       <div
         v-for="genre in sortedGenres"
         :key="genre.id"> 
          <h5>
-          <!-- <b-btn
-           :to= "{ name: 'GenreUpdate', params:{id : genre.id}}"
-           variant = "outline-primary">Edit</b-btn> -->
-        {{genre.nombre}} 
-
-        <!-- <router-link :to="{ name: 'GenreDetail', params: { id: genre.id } }"> -->
-       <!--  </router-link> -->
+          <b-btn
+           v-b-toggle="genre.nombre"
+           variant = "outline-success">Edit</b-btn>
+            {{genre.nombre}} 
+           <b-collapse :id="genre.nombre" class="mt-2">
+            <b-card>
+                Introduce new name:
+                <b-form-input
+                  id="nombre"
+                  v-model="genre.nombre"
+                  type="text"
+                  required/>
+                <b-btn
+                  variant="success"
+                  @click="save()">Submit</b-btn>
+                </b-card>
+          </b-collapse>
          </h5>
       </div>
     </div>
@@ -35,7 +58,8 @@ export default {
   data() {
     return {
       loading: false,
-      genres: null,
+      genres: [],
+      genre: {},
       error: null
     }
   },
@@ -51,6 +75,21 @@ export default {
     .then(response => this.genres = response.data)
     .catch(err => this.error = err.response.data)
     .finally(() => this.loading = false)
+  },
+  methods:{
+    save(){
+      if (this.$route.params.id) {
+        HTTP.put(`genres/${this.$route.params.id}`, this.genre)
+        .then(response =>
+          this.$router.replace({ name: 'GenreList', params: { id: response.data.id }}))
+        .catch(err => this.error = err.message)
+      } else {
+        HTTP.post('actors', this.genre)
+        .then(response =>
+          this.$router.replace({ name: 'GenreList', params: { id: response.data.id }}))
+        .catch(err => this.error = err.message)
+      }
+    }
   }
 }
 </script>
