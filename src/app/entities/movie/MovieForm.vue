021d5e22d000a55cd1c217236ac12aae2852a568
Jaime Iglesias <jaime.i.botas@gmail.com>
2018-12-07 22:32:35 +0100

    Movie Form Validation made pretty

--- src/app/entities/movie/MovieForm.vue
+++ src/app/entities/movie/MovieForm.vue
@@ -8,7 +8,8 @@
           @click="back()">Back</b-btn>
         <b-btn style="float left;"
           variant="outline-success"
-          @click="checkForm()">Submit</b-btn>
+          :disabled="$v.movie.$invalid"
+          @click="submitForm()">Submit</b-btn>
 
       <div class="float-right">
           <p style="margin:1%;">Ocultar</p>
@@ -18,75 +19,79 @@
       <b-form
         v-if="movie"
         class="app-form"
-        @submit="checkForm">
-
-        <p v-if="errors.length">
-          <b>Please, fix the following error(s):</b>
-          <ul>
-            <li v-for="error in errors"> {{ error }} </li>
-          </ul>
-        </p>
+        @submit="submitForm">
 
         <b-form-group
-          label="Titulo:"
-          label-for="title">
+          label="Title:"
+          label-for="title"
+          feedback="Title required">
           <b-form-input
             id="titulo"
             v-model="movie.titulo"
+            :state="!$v.movie.titulo.$invalid"
             type="text"
-            placeholder="Introduce titulo"/>
+            placeholder="Write movie title here"/>
         </b-form-group>
 
          <b-form-group class="form-group"
-          label="Pais:"
-          label-for="pais">
+          label="Country:"
+          label-for="Country"
+          feedback="Country required">
           <b-form-input
             id="pais"
             v-model="movie.pais"
+            :state="!$v.movie.pais.$invalid"
             placeholder="Introduce pais "/>
         </b-form-group>
 
         <b-form-group
           label="Productora:"
-          label-for="productora">
+          label-for="productora"
+          feedback="Producer required">
           <b-form-input
             id="productora"
             v-model="movie.productora"
+            :state="!$v.movie.productora.$invalid"
             placeholder="Introduce productora "/>
         </b-form-group>
 
          <b-form-group
           label="Duracion:"
-          label-for="duracion">
+          label-for="duracion"
+          feedback="Duration required">
           <b-form-input
             id="duracion"
+            type="number"
             v-model="movie.duracion"
+            :state="!$v.movie.duracion.$invalid"
             placeholder="Introduce duracion "/>
         </b-form-group>
 
         <b-form-group
           label="Director:"
-          label-for="director">
+          label-for="director"
+          feedback="You must choose a director from the list">
           <b-form-select
             id="director"
             :options="directors"
-            v-model="movie.director" />
+            v-model="movie.director"
+            :state="!$v.movie.director.$invalid" />
         </b-form-group>
 
         <b-form-group
           label = "Actores:"
           label-for="actores">
           <multiselect
-            class =multiselect_actores 
-            v-model="movie.actores" 
-            :options="allActors" 
-            :multiple="true" 
-            :close-on-select="false" 
-            :clear-on-select="false" 
+            class =multiselect_actores
+            v-model="movie.actores"
+            :options="allActors"
+            :multiple="true"
+            :close-on-select="false"
+            :clear-on-select="false"
             :preserve-search="true"
             :custom-label="customLabel"
             :show-labels="false"
-            placeholder="Pick some actors" 
+            placeholder="Pick some actors"
             label="nombre"
             track-by="nombre"
             :preselect-first="false">
@@ -96,19 +101,23 @@
 
          <b-form-group
           label="Genero:"
-          label-for="genero">
+          label-for="genero"
+          feedback="You must choose a genre">
           <b-form-select
             id="genero"
             :options="genres"
-            v-model="movie.genero" />
+            v-model="movie.genero"
+            :state="!$v.movie.genero.$invalid" />
         </b-form-group>
 
          <b-form-group
           label="Año salida:"
-          label-for="ano_salida">
+          label-for="ano_salida"
+          feedback="Release date required">
           <b-form-input
             id="ano_salida"
             v-model="movie.ano_salida"
+            :state="!$v.movie.ano_salida.$invalid"
             placeholder="Introduce año salida "/>
         </b-form-group>
 
@@ -124,10 +133,12 @@
 
         <b-form-group
           label="Sinopsis:"
-          label-for="sinopsis">
+          label-for="sinopsis"
+          feedback="Synopsis required">
           <b-form-textarea
             id="sinopsis"
             v-model="movie.sinopsis"
+            :state="!$v.movie.sinopsis.$invalid"
             :rows="3"
             :max-rows="6"
             placeholder="Introduce sinopsis"/>
@@ -151,7 +162,7 @@
                 <img v-bind:src= "movie.poster_path" v-on:load="isLoaded()" v-bind:class="{ active: isActive }">
               </div>
               <div class="description" style="float:right;">
-                <h3 class="titulo">  {{movie.original_title}} </h3> 
+                <h3 class="titulo">  {{movie.original_title}} </h3>
                 <p class="release">  {{movie.release_date}} </p>
               </div>
                <toggle-button v-model="movie.seleccionada"/>
@@ -192,6 +203,7 @@ import Multiselect from 'vue-multiselect'
 import theMovieDb from 'themoviedb-javascript-library'
 import PictureInput from 'vue-picture-input'
 import FormDataPost from '../../common/upload'
+import {required, integer, minValue} from 'vuelidate/lib/validators'
 
 export default {
   components: { LoadingPage,  Multiselect,  PictureInput},
@@ -199,7 +211,6 @@ export default {
     return {
       movie: {},
       movieSearch: [],
-      errors: [],
       error: null,
       image: null,
       isActive: false,
@@ -297,14 +308,14 @@ export default {
     getDirectors(){
       HTTP.get('directors')
       .then(response => this.allDirectors = response.data)
-      .catch(err => this.error = err.message)     
+      .catch(err => this.error = err.message)
     },
     getActors(){
        HTTP.get('actors')
       .then(response => this.allActors = response.data)
       .catch(err => this.error = err.message)
     },
-    save() {
+    submitForm() {
       if (this.$route.params.id) {
         HTTP.put(`movies/${this.$route.params.id}`, this.movie)
         .then(response =>
@@ -328,47 +339,44 @@ export default {
       var aux = json.results;
 
       aux.forEach(function (movie){
-          movie.poster_path = "https://image.tmdb.org/t/p/w300" + movie.poster_path 
+          movie.poster_path = "https://image.tmdb.org/t/p/w300" + movie.poster_path
           movie.seleccionada = false
         })
       this.movieSearch = aux;
       this.movie.ruta = "https://image.tmdb.org/t/p/w300" + json.results[0].poster_path;
-    },  
+    },
       errorCB: function (data) {
       console.log("Error callback: " + data);
-    },
-    checkForm: function() {
-      this.errors = [];
-
-      if(!this.movie.titulo){
-        this.errors.push("Title required");
+    }
+  },
+  validations: {
+    movie: {
+      titulo: {
+        required
+      },
+      sinopsis: {
+        required
+      },
+      director: {
+        required
+      },
+      genero: {
+        required
+      },
+      duracion: {
+        required,
+        integer
+      },
+      pais: {
+        required
+      },
+      productora: {
+        required
+      },
+      ano_salida: {
+        required,
+        integer
       }
-      if (!this.movie.sinopsis){
-        this.errors.push("Synopsis required");
-      }
-      if (!this.movie.director){
-        this.errors.push("Director required");      
-      }
-      if (!this.movie.genero){
-        this.errors.push("Genre required");       
-      }
-      if (!this.movie.duracion){
-        this.errors.push("Duration required");       
-      }
-      if (!this.movie.pais){
-        this.errors.push("Country required");        
-      }
-      if (!this.movie.productora){
-        this.errors.push("Producer required");        
-      }
-      if (!this.movie.ano_salida){
-        this.errors.push("Release year required");        
-      }
-
-      if (!this.errors.length) {
-        this.save();
-      }
-
     }
   }
 }
@@ -377,7 +385,11 @@ export default {
 <style lang="sass" src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style lang="scss">
 
-article { 
+article {
     display: block;
+}
+
+.invalidTitle{
+
 }
 </style>
